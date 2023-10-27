a20e439389db331b499bbca21232f39c7626819f
junios <junios@naver.com>
2023-10-27 13:46:41 +0900

    가짜 블랙잭 만들기

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include <iostream> 
+#include <algorithm>
 
 #define MAXCOUNT		52
+
+//black jack
 
 using namespace std;
 
 int main()
 {
 	int Pocket[MAXCOUNT]; // 0 + 1 ~ 51 + 1
+
+	//char CardsType[4] = { '♠', '♣', '♥', '◆' };
 
 	for (int i = 0; i < MAXCOUNT; ++i)
 	{
@@ -15,6 +20,8 @@ int main()
 
 	srand((unsigned int)time(nullptr));
 
+	//random_shuffle(Pocket[0], Pocket[52]);
+	//shuffle, 알고리즘, algorithm
 	for (int i = 0; i < 10000; ++i)
 	{
 		int First = rand() % MAXCOUNT;
@@ -24,10 +31,112 @@ int main()
 		Pocket[Second] = Temp;
 	}
 
-	for (int i = 0; i < 6; ++i)
+	int PickCards[4];
+
+	for (int i = 0; i < 4; ++i)
 	{
-		cout << Pocket[i] << endl;
+		PickCards[i] = Pocket[i];
 	}
+
+	int PlayerScore = 0;
+	int ComputerScore = 0;
+
+	//고른 카드 출력
+	for (int i = 0; i < 4; ++i)
+	{
+		int CardType = PickCards[i] / 13;
+
+		//cout << CardsType[CardType] << endl;
+		if (CardType == 0)
+		{
+			//Spade
+			cout << "♠ ";
+		}
+		else if (CardType == 1)
+		{
+			//Clover
+			cout << "♣ ";
+		}
+		else if (CardType == 2)
+		{
+			//Heart
+			cout << "♥ ";
+		}
+		else //(CardType == 3)
+		{
+			//Diamond
+			cout << "◆ ";
+		}
+
+		int CardNumber = (Pocket[i] % 13) + 1;
+		if (CardNumber == 11)
+		{
+			//J
+			cout << "J " << endl;
+		}
+		else if (CardNumber == 12)
+		{
+			//Q
+			cout << "Q " << endl;
+		}
+		else if (CardNumber == 13)
+		{
+			//K
+			cout << "K " << endl;
+		}
+		else
+		{
+			//숫자
+			cout << CardNumber << endl;
+		}
+
+		if (i < 2)
+		{
+			//Com
+			if (CardNumber > 10) // 11, 12, 13. J Q K
+			{
+				ComputerScore += 10;
+			}
+			else
+			{
+				ComputerScore += CardNumber;
+			}
+		}
+		else
+		{
+			//Player
+			if (CardNumber > 10) // 11, 12, 13. J Q K
+			{
+				PlayerScore += 10;
+			}
+			else
+			{
+				PlayerScore += CardNumber;
+			}
+		}
+	}
+
+	if (ComputerScore > 21)
+	{
+		cout << ComputerScore << " : " << PlayerScore << endl;
+		cout << "Player Win" << endl;
+	}
+	else if (PlayerScore > 21)
+	{
+		cout << ComputerScore << " : " << PlayerScore << endl;
+		cout << "Computer Win" << endl;
+	}
+	else if (ComputerScore > PlayerScore)
+	{
+		cout << ComputerScore << " : " << PlayerScore << endl;
+		cout << "Computer Win" << endl;
+	}
+	else
+	{
+		cout << ComputerScore << " : " << PlayerScore << endl;
+		cout << "Player Win" << endl;
+	}
+
 
 	return 0;
 }
